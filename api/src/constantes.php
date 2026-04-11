555e49f136e3a70682d449f146f8ef06a9fb28c9
Denis Albertini <denis.s.albertini@gmail.com>
2026-04-11 19:02:54 -0300

    Adiciona o tipo Cefetin

---
+++ api/src/constantes.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types=1);
+
+class MensagemErro
+{
+  public const string VALOR_INVALIDO_CEFETIN = 'O valor em Cefetin não pode ser menor que 0.';
+}
---
+++ api/src/tipos/Cefetin.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+class Cefetin
+{
+  private int $valorCentavos;
+
+  public function __construct(int $valorCentavos)
+  {
+    $this->setValorCentavos($valorCentavos);
+  }
+
+  private function setValorCentavos(int $valor)
+  {
+    if ($valor < 0) {
+      throw new DomainException(MensagemErro::VALOR_INVALIDO_CEFETIN);
+    }
+
+    $this->valorCentavos = $valor;
+  }
+
+  public function getValorFormatado(): string
+  {
+    $valorCentavosString = (string) $this->valorCentavos;
+
+    $parteInteira = substr($valorCentavosString, 0, -2);
+    $parteDecimal = substr($valorCentavosString, -2);
+
+    $valorFormatado = "$parteInteira,$parteDecimal";
+
+    return $valorFormatado;
+  }
+}
